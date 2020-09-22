a2b0c314371d692fe3a7730df447b986914fec56
Stuler <peter.jurek@gmail.com>
2020-09-22 19:34:53 +0200

    added workout list

--- backend.py
+++ backend.py
@@ -2,13 +2,15 @@ import datetime
 
 class Workout:
     '''Represents a workout in a logbook.'''
+    workouts = []
 
     def __init__(self, sport, workout_header, description):
-        self.workout_date = datetime.datetime()
+        self.workout_date = datetime.date.today()
         self.sport = sport 
         self.workout_header = workout_header
         self.description = description
         self.excercises = []
+        Workout.workouts.append(self)
 
     def add_excercise(self, excercise_name, excercise_load, 
     reps_done, serie_rpe, rest_int, note):
@@ -29,6 +31,7 @@ class Excercise:
 
 class WorkoutLog:
     '''Represents a logbook of workouts done by user.'''
+    
 
     def __init__(self):
         self.workouts = []
@@ -36,8 +39,10 @@ class WorkoutLog:
     def add_workout(self):
         self.workouts.append(Workout)
 
-#log = WorkoutLog()
-#workout = Workout("Fitness","Push day", "Focused on squats")
-#workout.add_excercise("Dumpbell press", 35, 6, 8, 2.5, "Felt good")
-#workout.add_excercise("Squat", 92.5, 4, 9, 3.5, "Felt really hard, good slow" 
-#" technique")
+log = WorkoutLog()
+workout = Workout("Fitness","Push day", "Focused on squats")
+workout.add_excercise("Dumpbell press", 35, 6, 8, 2.5, "Felt good")
+workout.add_excercise("Squats", 92.5, 4, 9, 3.5, "Felt really hard, good slow" 
+" technique")
+
+print(workout.workouts[0].excercises[1].excercise_name)
