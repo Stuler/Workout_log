a10126451f7cf836352497538699bb7f8cc86704
Stuler <peter.jurek@gmail.com>
2020-09-30 20:54:15 +0200

    some work on command menu

--- command_menu.py
+++ command_menu.py
@@ -8,7 +8,7 @@ class Menu:
         self.database = Database("workout_log.db")
         self.choices = {
             "1": self.show_workouts,
-            "2": self.add_wkout,
+            "2": self.add_wkout_menu,
             "3": self.modify_workout,
             "4": self.delete_workout,
             "5": self.quit
@@ -38,17 +38,35 @@ class Menu:
     def show_workouts(self):
         pass
 
+    def add_wkout_menu(self):
+        self.wkout_choices = {
+            "1": 
+        }
+
+    def add_wkout_menu_display(self):
+        print('''
+            1. Add new excercise
+            2. Modify excercise
+            3. Quit adding excercises / finish workout
+        ''')
+
     def add_wkout(self):
-        id = input("Number of an excercise: ")
-        excercise_name = input("Excercise: ")
-        excercise_load = input("Excercise load: ")
-        reps_done = input("Repeats done: ")
-        serie_rpe = input("RPE of an effort: ")
-        rest_int = input("Length of rest interval: ")
-        note = input("Additional note: ")
-        self.database.insert_workout(id, excercise_name, excercise_load, 
-                                    reps_done, serie_rpe, rest_int, note)
-        print("Excercise has been added to the workout!")
+        wkout = "Active"
+        while wkout == "Active":
+            prompt = input("")
+            id = input("Number of an excercise: ")
+            excercise_name = input("Excercise: ")
+            excercise_load = input("Excercise load: ")
+            reps_done = input("Repeats done: ")
+            serie_rpe = input("RPE of an effort: ")
+            rest_int = input("Length of rest interval: ")
+            note = input("Additional note: ")
+            self.database.insert_workout(id, excercise_name, excercise_load, 
+                                        reps_done, serie_rpe, rest_int, note)
+            print("Excercise has been added to the workout!")
+
+    def add_exc(self):
+        pass
 
     def modify_workout(self):
         pass
